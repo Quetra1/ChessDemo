f17f3b030e007ce0a6bda1851a0640ca0a045389
Quetra1 <lmorlandallen1@gmail.com>
2022-04-14 13:02:13 +0100

    Set up board in engine

---
+++ engine.py
@@ -0,0 +1,23 @@
+# Responsible for storing current game state, determining valid moves
+class GameState():
+    def __init__(self):
+        self.starting_board()
+        self.current_turn = 0
+        self.check = False
+        self.selection = False
+
+    # 8x8 2D list represents the board
+    # First character represents Black/White
+    # Second character represents the piece type R, N, B, K, P
+    # -- Represents empty space
+    def starting_board(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
