d122d849191e004b8d2e175b9d7fa86b6fd80756
Quetra1 <lmorlandallen1@gmail.com>
2022-04-17 15:07:59 +0100

    Added pawn move, take, and 2 squares intiial move rule (no en passant)

--- engine.py
+++ engine.py
@@ -1,4 +1,7 @@
 # Responsible for storing current game state, determining valid moves
+from re import I
+
+
 class GameState():
     def __init__(self):
         self.starting_board()
@@ -39,6 +42,15 @@ class GameState():
     def generate_valid_moves(self, square_x, square_y, selected_piece, selected_piece_colour):
         selected_piece_type = selected_piece[1]
         self.valid_moves = []
+
+        # Pawn
+        if selected_piece_type == "P":
+            # White pawn
+            if selected_piece_colour == "w":
+                self.validate_pawn_move(square_x, square_y, -1, +1, 6, selected_piece_colour)
+            # Black pawn
+            else:
+                self.validate_pawn_move(square_x, square_y, +1, -1, 1, selected_piece_colour)
         # Knight
         if selected_piece_type == "N":
             self.validate_knight(square_x, square_y, selected_piece_colour)     
@@ -60,6 +72,94 @@ class GameState():
             self.check_line(directions, 1, square_x, square_y, selected_piece_colour)
         
         return self.valid_moves
+
+    def validate_pawn_take(self, square_y, square_x, selected_piece_colour):
+        # Skip check if diaganal is off the board
+        if square_y < 0 or square_x < 0 or square_y > 7 or square_x > 7:
+            return
+        else:
+            # Simulates the move and then checks if the move would result in check for the moving player's king.
+            # If it does, move is not valid
+            #if not self.simulate_move(square_x, square_y):
+                # Add to valid moves there is a piece on the diaganal square and the piece is a different colour
+                piece = self.board[square_y][square_x]
+                piece_colour = piece[0]
+                if piece != "--" and piece_colour != selected_piece_colour:
+                    self.valid_moves.append((square_y, square_x))
+
+    def validate_pawn_move(self, square_x, square_y, a, b, starting_row, selected_piece_colour):
+        # Check for pieces on diagnal to take
+        self.validate_pawn_take(square_x + a, square_y + a, selected_piece_colour)
+        self.validate_pawn_take(square_x + a, square_y + b, selected_piece_colour)
+
+        # Check if pawn is on starting position, if so allow double move
+        if square_x == starting_row:
+            x = 3
+        else:
+            x = 2
+
+        for i in range(1, x):
+            new_square_x = square_x + (i * a)
+            print("Original Coordinates: " + str(square_x) + "," + str(square_y))
+            print("New Coordinates: " + str(new_square_x) + "," + str(square_y))
+            piece = self.board[new_square_x][square_y]
+            # Simulates the move and then checks if the move would result in check for the moving player's king.
+            # If it does, move is not valid - skip to next move
+            #if self.simulate_move(self.old_square_x, square_y):
+            #    continue
+
+            # If there is a piece in the way, we can't move there - return before added to valid moves
+            if piece != "--":
+                return
+            else:
+                self.valid_moves.append((new_square_x, square_y))
+
+    def validate_w_pawn_move(self):
+
+        # Check for pieces on diagnal to take
+        self.validate_pawn_take(self.old_posY - 1, self.old_posX - 1)
+        self.validate_pawn_take(self.old_posY - 1, self.old_posX + 1)
+
+        # Check if pawn is on starting position, if so allow double move
+        if self.old_posY == 6:
+            x = 2
+        else:
+            x = 1
+        for i in range(x):
+            posY = self.old_posY - (i + 1)
+            piece = self.board[posY][self.old_posX]
+            # Simulates the move and then checks if the move would result in check for the moving player's king.
+            # If it does, move is not valid - skip to next move
+            if self.simulate_move(self.old_posX, posY):
+                continue
+            # If there is a piece in the way, we can't move there - return before added to valid moves
+            if piece != "--":
+                return
+            else:
+                self.valid_list.append((posY, self.old_posX))
+
+    def validate_b_pawn_move(self):
+        # Check for pieces on diagnal to take
+        self.validate_pawn_take(self.old_posY + 1, self.old_posX + 1)
+        self.validate_pawn_take(self.old_posY + 1, self.old_posX - 1)
+
+        # Check if pawn is on starting position, if so allow double move
+        if self.old_posY == 1:
+            x = 2
+        else:
+            x = 1
+        for i in range(x):
+            posY = self.old_posY + (i + 1)
+            piece = self.board[posY][self.old_posX]
+            # Simulates the move and then checks if the move would result in check for the moving player's king.
+            # If it does, move is not valid - skip to next move
+            if self.simulate_move(self.old_posX, posY):
+                continue
+            # If there is a piece in the way, we can't move there - return before added to valid moves
+            if piece != "--":
+                return
+            else:
+                self.valid_list.append((posY, self.old_posX))
 
     def validate_knight(self, square_x, square_y, selected_piece_colour):
         # Iterate through a set of coordinates to check if every possible move is valid
